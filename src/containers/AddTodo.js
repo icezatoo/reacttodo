776ba503a2de034b4cd384357fec911801393c24
icezatoo <ice.pp@hotmail.com>
2018-08-30 06:01:29 +0700

    connect todo list

--- src/containers/AddTodo.js
+++ src/containers/AddTodo.js
@@ -1,7 +1,8 @@
 import React, { PureComponent } from 'react';
 import { Button, Form, FormGroup, Input, Row, Col } from 'reactstrap';
 import styled from 'styled-components';
-
+import { connect } from 'react-redux';
+import { AddTodolist } from '../common/actions/todo';
 const AddTodoContainer = styled.div`
   display: flex;
   align-items: center;
@@ -9,21 +10,44 @@ const AddTodoContainer = styled.div`
 `;
 
 class AddTodo extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    const { value } = this.state;
+    const { dispatch } = this.props;
+    dispatch(AddTodolist(value));
+    this.setState({ value: '' });
+    event.preventDefault();
+  }
+
   render() {
+    const { value } = this.state;
+    const disabledbtn = value == null || value == '';
     return (
       <Row>
         <Col sm="12" md={{ size: 8, offset: 2 }}>
           <AddTodoContainer>
-            <Form inline>
+            <Form inline onSubmit={this.handleSubmit}>
               <FormGroup>
                 <Input
                   type="text"
                   name="name"
                   id="name"
+                  value={value}
+                  onChange={this.handleChange}
                   placeholder="Add name"
                 />
               </FormGroup>
-              <Button>Add</Button>
+              <Button disabled={disabledbtn}>Add</Button>
             </Form>
           </AddTodoContainer>
         </Col>
@@ -32,4 +56,4 @@ class AddTodo extends PureComponent {
   }
 }
 
-export default AddTodo;
+export default connect()(AddTodo);
